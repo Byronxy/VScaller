aa8db480bde2d4316ead344b1bd27a6e47ce2fe0
Yi Xiong <390479311@qq.com>
2020-09-20 20:16:45 +0800

    v0.1.0

---
+++ R/zzz.R
@@ -0,0 +1,7 @@
+##' @importFrom utils packageDescription
+.onAttach <- function(libname, pkgname) {
+  pkgVersion <- packageDescription(pkgname, fields="Version")
+  msg <- paste0(pkgname, " v", pkgVersion, "  ",
+                "Welcome to use ", pkgname, "\n\n")
+  packageStartupMessage(paste0(msg))
+}
